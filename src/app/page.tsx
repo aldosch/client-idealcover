7b80dd167ae51d313c505d3fed67958acc814ba3
aldo <github@aldo.io>
2023-09-05 13:08:17 +1000

    add prismic content import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>test</h1>
-    </main>
-  );
+import { Metadata } from "next";
+import { repositoryName } from "../prismicio";
+import { SliceZone } from "@prismicio/react";
+import { createClient } from "@prismicio/client";
+import { components } from "../slices";
+
+export default async function Home() {
+  const client = createClient(repositoryName);
+  const page = await client.getSingle("home");
+
+  return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient(repositoryName);
+  const page = await client.getSingle("home");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
