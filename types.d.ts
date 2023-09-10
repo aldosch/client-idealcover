65804ff42d7dba1a68d7a962c943bc1fc195bfc8
aldo <github@aldo.io>
2023-09-10 15:03:57 +1000

    add custom types

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+export type menuItem = {
+  label: string;
+  link: any;
+};
+
+export type socialItem = {
+  platform: "Facebook" | "Instagram" | "Linkedin" | "Twitter" | "Youtube";
+  link: any;
+};
